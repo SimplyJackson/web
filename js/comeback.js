5b19c6fc70c9661459326d8cd7d91a8d486c6b50
Simply Jackson <jackson.canfield2016@gmail.com>
2024-05-15 15:21:00 +0000

    changes

---
+++ js/comeback.js
@@ -0,0 +1,23 @@
+let title = document.title;
+let intervalTimer = null;
+let sayings = [
+    'Come back!',
+    'Where\'d ya go?',
+    'Don\'t leave!'
+];
+let sayingIndex = 0;
+window.addEventListener('blur', function() {
+    intervalTimer = setInterval(function() {
+        document.title = sayings[sayingIndex];
+        sayingIndex++
+        if (sayingIndex >= sayings.length) sayingIndex = 0;
+    }, 6000);
+});
+window.addEventListener('focus', function() {
+    if (intervalTimer === null || document.title === title) return;
+    clearInterval(intervalTimer);
+    setTimeout(function() {
+        document.title = title;
+    }, 2000);
+    document.title = '🙂 Welcome back!';
+});
